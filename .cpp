62772e503b712e153586bdb5addd086e73eb9627
Asna Majeed <85230276+Asna1408@users.noreply.github.com>
2022-02-10 11:04:52 +0530

    Create .cpp

---
+++ .cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+
+    double a,b;
+
+    int x;
+
+    cout << "Please select your operation to be implemented\n";
+
+    cout << "Enter:\n";
+
+    cout <<"1 for Addition\n";
+
+    cout <<"2 for substraction\n";
+
+    cout <<"3 for multiplication\n";
+
+    cout <<"4 for division\n";
+
+    cin >>x;
+
+    cout <<"Enter two numbers to implement this operation:\n";
+
+    cin >>a>>b;
+
+    switch(x)
+
+    {
+
+        case 1:
+
+        cout<<"You have selected addition operation\n";
+
+        cout<<"Result is: "<<a+b;
+
+        break;
+
+        case 2:
+
+        cout<<"You have selected substraction operation\n";
+
+        cout<<"Result is: "<<a-b;
+
+        break;
+
+        case 3:
+
+        cout<<"You have selected multiplication operation\n";
+
+        cout<<"Result is: "<<a*b;
+
+        break;
+
+        case 4:
+
+        cout<<"You have selected division operation\n";
+
+        cout<<"Result is: "<<a/b;
+
+        break;
+
+    }
+
+    return 0;
+
+    }
